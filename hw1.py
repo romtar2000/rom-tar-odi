d59f722f03f826708329a0db46068bdcb6fce74f
romtar2000 <31881535+romtar2000@users.noreply.github.com>
2017-09-25 17:38:16 +0300

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,7 +1,7 @@
 a= int (input ("Введите 1-ое число") )
 b= int (input ("Введите 2-ое число") )
 c= int (input ("Введите 3-е число") )
-if (b != 0) and (a % b == c) and (a / b == c):
- print ("YES")
+ if (b != 0) and (a % b == c) and (a / b == c):
+  print ("YES")
 else:
- print ("NO")
+  print ("NO")
