6d27b9d4f4911ff1607bb36737e4f68c05cce618
romtar2000 <rvtarasov@edu.hse.ru>
2017-10-01 10:15:47 +0300

    send homework

--- hw1.py
+++ hw1.py
@@ -2,6 +2,6 @@ a= int (input ("Введите 1-ое число") )
 b= int (input ("Введите 2-ое число") )
 c= int (input ("Введите 3-е число") )
 if (b != 0) and (a % b == c) and (a / b == c):
- print ("YES")
+  print ("YES")
 else:
- print ("NO")
+  print ("NO")
