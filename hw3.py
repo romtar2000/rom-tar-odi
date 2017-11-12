d941986aded30ae4fdb2b98ed874ed9e57863711
romtar2000 <31881535+romtar2000@users.noreply.github.com>
2017-11-12 12:11:16 +0300

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,9 @@
+word=list(input("Enter word: "))
+if word:
+    for i,sym in enumerate(word):
+        word=word[:-1]
+        word1="".join(word)
+        print(word1)
+    print ("No more symbols")
+else:
+    print ("Enter at least one symbol")
