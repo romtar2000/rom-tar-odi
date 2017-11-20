edfc04efe3650c5a07b92c6dac7e2a8eceed6afd
romtar2000 <31881535+romtar2000@users.noreply.github.com>
2017-11-20 22:09:20 +0300

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,57 @@
+print ("1st variant")
+with open ("C:/Users/1003746/Desktop/hw4.txt", encoding="utf-8") as f:
+   lines = f.readlines()
+   value = len(lines)
+   print(value)
+   firstline = lines[0]
+   words1 = firstline.split()
+   value1 = len(words1)
+   print(value1)
+   secondline = lines[1]
+   words2 = secondline.split()
+   value2 = len(words2)
+   print(value2)
+   thirdline = lines[2]
+   words3 = thirdline.split()
+   value3 = len(words3)
+   print(value1)
+   fourthline = lines[3]
+   words4 = fourthline.split()
+   value4 = len(words4)
+   print(value4)
+   fifthline = lines[4]
+   words5 = fifthline.split()
+   value5 = len(words5)
+   print(value5)
+   sixthline = lines[5]
+   words6 = sixthline.split()
+   value6 = len(words6)
+   print(value6)
+   seventhline = lines[6]
+   words7 = seventhline.split()
+   value7 = len(words7)
+   print(value7)
+   eigthline = lines[7]
+   words8 = eigthline.split()
+   value8 = len(words8)
+   print(value8)
+   ninethline = lines[8]
+   words9 = ninethline.split()
+   value9 = len(words9)
+   print(value9)
+   tenthline = lines[9]
+   words10 = tenthline.split()
+   value10 = len(words10)
+   print(value10)
+   eleventhline = lines[10]
+   words11 = eleventhline.split()
+   value11 = len(words11)
+   print(value11)
+   twelvethline = lines[11]
+   words12 = twelvethline.split()
+   value12 = len(words12)
+   print(value12)
+   averagelen = (value1+value2+value3+value4+value5+value6+value7+value8+value9+value10+value11+value12)/12
+   print(averagelen)
+   
+   
