d830698318a8288bed078db375cdd2e54d2cac2c
romtar2000 <31881535+romtar2000@users.noreply.github.com>
2017-12-06 23:40:25 +0300

    Update ha5.py

--- ha5.py
+++ ha5.py
@@ -10,7 +10,7 @@ with open ("proga.txt", "w", encoding="utf-8") as f:
     for element in range(len(list)):
         word = list[element]
         u = ""
-        for item in range(len(word -1, -1, -1)):
+        for item in range(len(word) -1, -1, -1):
             u = u+word[item]
         for item in range(len(u)):
             if((item+1)%3) != 0:
