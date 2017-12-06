c1ef43373e3491117310d34c169e4cf8c22a8665
romtar2000 <31881535+romtar2000@users.noreply.github.com>
2017-12-06 23:34:16 +0300

    Add files via upload

---
+++ ha5.py
@@ -0,0 +1,20 @@
+slovo = input("Enter word: ")
+list= []
+if slovo == "":
+    print("No word entered")
+else:
+    while slovo != "":
+        list.append(slovo)
+        slovo = input("Enter word: ")
+with open ("proga.txt", "w", encoding="utf-8") as f:
+    for element in range(len(list)):
+        word = list[element]
+        u = ""
+        for item in range(len(word -1, -1, -1)):
+            u = u+word[item]
+        for item in range(len(u)):
+            if((item+1)%3) != 0:
+                f.write(u[element])
+        f.write("\n")
+
+    
